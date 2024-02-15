07e4986ec6900ccaad1d985f1504ee47a49a7bd8
Phoquiche <dustybertin@gmail.com>
2024-02-15 15:45:37 +0100

    Change Package name to User

--- src/main/java/User/GachaApiMtApplication.java
+++ src/main/java/User/GachaApiMtApplication.java
@@ -1,4 +1,4 @@
-package gachagame;
+package User;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/User/GachaApiMtApplicationTests.java
+++ src/test/java/User/GachaApiMtApplicationTests.java
@@ -1,4 +1,4 @@
-package gachagame;
+package User;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
