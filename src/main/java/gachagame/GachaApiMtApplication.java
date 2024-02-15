efbe5d8fff6247f8942eda02bc42e67e8b3b0910
Phoquiche <dustybertin@gmail.com>
2024-02-15 14:45:25 +0100

    Initial commit

---
+++ src/main/java/gachagame/GachaApiMtApplication.java
@@ -0,0 +1,13 @@
+package gachagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaApiMtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachaApiMtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gachagame/GachaApiMtApplicationTests.java
@@ -0,0 +1,13 @@
+package gachagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaApiMtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
