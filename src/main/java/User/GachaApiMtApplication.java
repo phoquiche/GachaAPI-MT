4c3d87f8f35946fb7e1a4da240a576fbefe87896
phoquiche <dustybertin@gmail.com>
2024-02-18 16:10:16 +0100

    Test sur l'accès a distance

--- src/main/java/User/GachaApiMtApplication.java
+++ src/main/java/User/GachaApiMtApplication.java
@@ -2,6 +2,7 @@ package User;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GachaApiMtApplication {
@@ -9,5 +10,10 @@ public class GachaApiMtApplication {
     public static void main(String[] args) {
         SpringApplication.run(GachaApiMtApplication.class, args);
     }
+
+    @RestController
+
+
+
 
 }
