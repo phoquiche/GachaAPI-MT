fb068003068696e89ee8e0055423c2912aeb1b6e
Phoquiche <dustybertin@gmail.com>
2024-02-22 14:52:10 +0100

    Ajout de méthodes pour gérer le level up

--- src/main/java/User/GachaApiMtApplication.java
+++ src/main/java/User/GachaApiMtApplication.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
@@ -26,10 +27,59 @@ public class GachaApiMtApplication {
     }
 
     public static MongoCollection<Document> getMongo(String collectionName) {
-        MongoClient mongoClient = MongoClients.create("mongodb://www.soybank.fr:27017");
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase database = mongoClient.getDatabase("gacha");
         MongoCollection<Document> collection = database.getCollection(collectionName);
         return collection;
+    }
+
+
+    public static boolean checkLevelUp(int idJoueur){
+        MongoCollection<Document> collection = GachaApiMtApplication.getMongo("User");
+        Document doc = collection.find(Filters.eq("id", idJoueur)).first();
+        assert doc != null;
+        int level = doc.getInteger("lvl");
+        int xp = doc.getInteger("exp");
+         if (xp>=(int)(Math.pow(1.1, level)*50)){
+            levelUp(idJoueur);
+            return true;
+        }
+         else return false;
+
+    }
+
+    public static void levelUp(int idJoueur){
+        MongoCollection<Document> collection = GachaApiMtApplication.getMongo("User");
+        //convert idJoueur to string
+        Document doc = collection.find(Filters.eq("id", idJoueur)).first();
+        assert doc != null;
+        int level = doc.getInteger("lvl");
+        int xp = doc.getInteger("exp");
+        int newLevel = level+1;
+        int newXP = xp-(int)(Math.pow(1.1, level)*50);
+        Document update = new Document("lvl", newLevel).append("exp", newXP);
+        collection = GachaApiMtApplication.getMongo("User");
+        collection.updateOne(Filters.eq("id", idJoueur), new Document("$set", update));
+
+
+    }
+    public static boolean addXP(int idJoueur, int xp){
+        MongoCollection<Document> collection = GachaApiMtApplication.getMongo("User");
+        Document doc = collection.find(Filters.eq("id", idJoueur)).first();
+        assert doc != null;
+        int level = doc.getInteger("lvl");
+        int newXP = doc.getInteger("exp")+xp;
+        Document update = new Document("exp", newXP);
+        collection = GachaApiMtApplication.getMongo("User");
+        collection.updateOne(Filters.eq("id", idJoueur), new Document("$set", update));
+        return checkLevelUp(idJoueur);
+    }
+
+    public static String afficheInfoJSON(int idJoueur){
+        MongoCollection<Document> collection = GachaApiMtApplication.getMongo("User");
+        Document doc = collection.find(Filters.eq("id", idJoueur)).first();
+        assert doc != null;
+        return doc.toJson();
     }
 
 
@@ -43,11 +93,12 @@ public class GachaApiMtApplication {
 class GachaController {
     @GetMapping("/gacha")
     public String gacha() {
+       GachaApiMtApplication.addXP(001,100);
+       return GachaApiMtApplication.afficheInfoJSON(001);
 
-       MongoCollection<Document> collection = GachaApiMtApplication.getMongo("User");
-       Document doc = collection.find().first();
-        assert doc != null;
-        return doc.toJson();
+
+
+
 
     }
 }
